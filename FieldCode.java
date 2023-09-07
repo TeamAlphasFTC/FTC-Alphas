50d95b6424e2336fda8cba28bd966c17dee2e6ef
Sahas Sandesh <139258826+sahas-sandesh@users.noreply.github.com>
2023-09-07 12:34:54 +0530

    Update FieldCode.java

--- FieldCode.java
+++ FieldCode.java
@@ -1,10 +1,11 @@
-package org.firstinspires.ftc.robotcontroller;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-public class M extends LinearOpMode {
+@TeleOp(name="FieldCODE")
+public class FieldCODE extends LinearOpMode {
     @Override
     public void runOpMode(){
         DcMotor rightRear;
